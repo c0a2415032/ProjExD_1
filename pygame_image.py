cca87ea0fc6990b227f4d940bad608e1eee03775
c0a2415032 <c0a2415032@edu.teu.ac.jp>
2025-04-15 16:25:30 +0900

    練習10

--- pygame_image.py
+++ pygame_image.py
@@ -12,6 +12,11 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bg_img2 = pg.image.load("fig/pg_bg.jpg")#練習８
     kk_img = pg.image.load("fig/3.png")#練習２
+    kk_rct = kk_img.get_rect()#レクトの取得　練習１０
+    kk_rct.center = 300, 200#中心座標
+
+
+
     kk_img = pg.transform.flip(kk_img, True, False)
     bg_img2 = pg.transform.flip(bg_img, True, False)#練習８
 
@@ -20,13 +25,17 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        key_lst = pg.key.get_pressed()#・キーの押下状態リストを取得
+        
+
     
         x = tmr%3200
         screen.blit(bg_img, [-x, 0])#練習６
         screen.blit(bg_img2, [-x+1600, 0])#練習７ 
         screen.blit(bg_img, [-x+3200, 0])#練習９
+        screen.blit(kk_img, kk_rct)#練習４ １０
 
-        screen.blit(kk_img, [300, 200])#練習４
+
         pg.display.update()
         tmr += 1
       
