b51706eccb9c92e9b233a20d96a195616bba7e51
c0a2415032 <c0a2415032@edu.teu.ac.jp>
2025-04-15 16:02:27 +0900

    練習8背景画像反転接続

--- pygame_image.py
+++ pygame_image.py
@@ -10,8 +10,10 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.image.load("fig/pg_bg.jpg")#練習８
     kk_img = pg.image.load("fig/3.png")#練習２
     kk_img = pg.transform.flip(kk_img, True, False)
+    bg_img2 = pg.transform.flip(bg_img, True, False)#練習８
 
     tmr = 0
     x = tmr#練習６
@@ -21,7 +23,7 @@ def main():
     
         x = tmr
         screen.blit(bg_img, [-x, 0])#練習６
-        screen.blit(bg_img, [-x+1600, 0])#練習７
+        screen.blit(bg_img2, [-x+1600, 0])#練習７
         screen.blit(kk_img, [300, 200])#練習４
         pg.display.update()
         tmr += 1
